d949a8481ef64f0c34e89cadbf3a6484eab45c3d
TseleMolelekoa <molelekoa58@gmail.com>
2024-04-25 16:55:42 +0200

    space removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/styles/main.css'; // Import custom CSS styles
 import HomePage from './components/homepage';
@@ -16,7 +16,7 @@ function App() {
         <div>
             <Navbar variant="dark" expand="xxl" className="navbar" style={{top: 0, zIndex: 100, color: 'white', }}>
                 {/* Logo as Navbar Brand */}
-                <Navbar.Brand href="#"><h3>Early Rise Security Services</h3>
+                <Navbar.Brand href="#" style={{alignContent:"center"}}><h3>Early Rise Security Services</h3>
 
                 </Navbar.Brand>
                 <p>We provide professional security services tailored to your needs, safety and peace of
@@ -24,8 +24,7 @@ function App() {
             </p>
 
                 {/* Navbar Toggle Button */}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" style={{alignContent:"center"}}/>
-
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 {/* Navbar Links */}
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="ms-auto"> {/* 'ms-auto' to align items to the right */}
@@ -54,7 +53,7 @@ function App() {
                     <p>&copy; 2024 Early Rise Security</p>
                     {/* Social Media Links */}
                     <div className="social-links">
-                        <a href="https://www.facebook.com/example" target="_blank" rel="noopener noreferrer">
+                        <a href="https://web.facebook.com/profile.php?id=61559048094131" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faFacebook} size="2x" className="mx-2"/>
                         </a>
                         <a href="https://www.twitter.com/example" target="_blank" rel="noopener noreferrer">
@@ -63,8 +62,8 @@ function App() {
                         <a href="https://www.instagram.com/example" target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faInstagram} size="2x" className="mx-2"/>
                         </a>
-                        <a href="https://www.linkedin.com/company/example" target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faLinkedin} size="2x" className="mx-2"/>
+                        <a href="https://wa.me/0615192283/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faWhatsapp} size="2x" className="mx-2"/>
                         </a>
                         <p>Developed by: <a href="https://tselemolelekoa.web.app" >Tsele Molelekoa</a></p>
                     </div>
